3625ea580e17187aa06ebf02ac44f7fecb295fcb
Tristan Ader <tristanader@gmail.com>
2014-09-22 02:16:50 -0500

    stubbing out hero carousel

---
+++ page-home.php
@@ -0,0 +1,13 @@
+<?php
+
+/** 
+ * Homepage
+ */
+
+get_header(); ?>
+<div class="l-hero-con">
+	<div class="l-hero">
+		<img src="<?php bloginfo('template_directory');?>/img/temp-hero-1.jpg" alt="">
+	</div>
+</div>
+<?php get_footer(); ?>
