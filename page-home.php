f73666af2c482d4e179747b0c9705a2241f282ab
Tristan Ader <tristanader@gmail.com>
2014-09-23 10:07:59 -0500

    first pass at icons ctas

--- page-home.php
+++ page-home.php
@@ -20,68 +20,76 @@ get_header(); ?>
 	</article>
 </div>
 <div class="l-cta-icons-con l-outer">
-	<section class="l-inner cta-icons">
-		<h3><?php the_field('home_practice_areas_heading'); ?></h3>
-		<section class="l-cta-icon-col cta-icon">
-			<?php
-				$svg = get_field('home_practice_area_col1_svg');
-				$img = get_field('home_practice_area_col1_default_image');
-				if ($img['alt']) {
-					$alt = $img['alt'];
-				} else {
-					$alt = get_field('home_practice_area_col1_heading');
-				}
-			?>
-			<?php if ($svg) : ?>
-				<img src="<?php echo $svg['url'] ?>" onerror="this.src='<?php echo $img['url'] ?>'" alt="<?php echo $alt ?>">
-			<?php else : ?>
-				<img src="<?php echo $img['url'] ?>" alt="<?php echo $alt; ?>">
-			<?php endif; ?>
-			<h4><?php the_field('home_practice_area_col1_heading'); ?></h4>
-			<?php the_field('home_practice_area_col1_content'); ?>
-			<p><a href="<?php the_field('home_practice_area_col1_link'); ?>"><?php the_field('home_practice_area_col1_link_text'); ?></a></p>
+	<section class="l-inner cta-icons clearfix">
+		<h3 class="cta-icons-main-heading"><?php the_field('home_practice_areas_heading'); ?></h3>
+		<div class="l-cta-icons-wrapper clearfix">
+			<section class="l-cta-icon-col cta-icon">
+				<?php
+					$svg = get_field('home_practice_area_col1_svg');
+					$img = get_field('home_practice_area_col1_default_image');
+					if ($img['alt']) {
+						$alt = $img['alt'];
+					} else {
+						$alt = get_field('home_practice_area_col1_heading');
+					}
+				?>
+				<div class="img-holder">
+					<?php if ($svg) : ?>
+						<img src="<?php echo $svg['url'] ?>" onerror="this.src='<?php echo $img['url'] ?>'" alt="<?php echo $alt ?>">
+					<?php else : ?>
+						<img src="<?php echo $img['url'] ?>" alt="<?php echo $alt; ?>">
+					<?php endif; ?>
+				</div>
+				<h4><?php the_field('home_practice_area_col1_heading'); ?></h4>
+				<?php the_field('home_practice_area_col1_content'); ?>
+				<p class="cta-icon-link"><a href="<?php the_field('home_practice_area_col1_link'); ?>"><?php the_field('home_practice_area_col1_link_text'); ?></a></p>
 
-		</section>
-		<section class="l-cta-icon-col cta-icon">
-			<?php
-				$svg = get_field('home_practice_area_col2_svg');
-				$img = get_field('home_practice_area_col2_default_image');
-				if ($img['alt']) {
-					$alt = $img['alt'];
-				} else {
-					$alt = get_field('home_practice_area_col2_heading');
-				}
-			?>
-			<?php if ($svg) : ?>
-				<img src="<?php echo $svg['url'] ?>" onerror="this.src='<?php echo $img['url'] ?>'" alt="<?php echo $alt ?>">
-			<?php else : ?>
-				<img src="<?php echo $img['url'] ?>" alt="<?php echo $alt; ?>">
-			<?php endif; ?>
-			<h4><?php the_field('home_practice_area_col2_heading'); ?></h4>
-			<?php the_field('home_practice_area_col2_content'); ?>
-			<p><a href="<?php the_field('home_practice_area_col2_link'); ?>"><?php the_field('home_practice_area_col2_link_text'); ?></a></p>
+			</section>
+			<section class="l-cta-icon-col cta-icon">
+				<?php
+					$svg = get_field('home_practice_area_col2_svg');
+					$img = get_field('home_practice_area_col2_default_image');
+					if ($img['alt']) {
+						$alt = $img['alt'];
+					} else {
+						$alt = get_field('home_practice_area_col2_heading');
+					}
+				?>
+				<div class="img-holder">
+					<?php if ($svg) : ?>
+						<img src="<?php echo $svg['url'] ?>" onerror="this.src='<?php echo $img['url'] ?>'" alt="<?php echo $alt ?>">
+					<?php else : ?>
+						<img src="<?php echo $img['url'] ?>" alt="<?php echo $alt; ?>">
+					<?php endif; ?>
+				</div>
+				<h4><?php the_field('home_practice_area_col2_heading'); ?></h4>
+				<?php the_field('home_practice_area_col2_content'); ?>
+				<p  class="cta-icon-link"><a href="<?php the_field('home_practice_area_col2_link'); ?>"><?php the_field('home_practice_area_col2_link_text'); ?></a></p>
 
-		</section>
-		<section class="l-cta-icon-col cta-icon">
-			<?php
-				$svg = get_field('home_practice_area_col3_svg');
-				$img = get_field('home_practice_area_col3_default_image');
-				if ($img['alt']) {
-					$alt = $img['alt'];
-				} else {
-					$alt = get_field('home_practice_area_col1]3_heading');
-				}
-			?>
-			<?php if ($svg) : ?>
-				<img src="<?php echo $svg['url'] ?>" onerror="this.src='<?php echo $img['url'] ?>'" alt="<?php echo $alt ?>">
-			<?php else : ?>
-				<img src="<?php echo $img['url'] ?>" alt="<?php echo $alt; ?>">
-			<?php endif; ?>
-			<h4><?php the_field('home_practice_area_col3_heading'); ?></h4>
-			<?php the_field('home_practice_area_col3_content'); ?>
-			<p><a href="<?php the_field('home_practice_area_col3_link'); ?>"><?php the_field('home_practice_area_col3_link_text'); ?></a></p>
+			</section>
+			<section class="l-cta-icon-col cta-icon">
+				<?php
+					$svg = get_field('home_practice_area_col3_svg');
+					$img = get_field('home_practice_area_col3_default_image');
+					if ($img['alt']) {
+						$alt = $img['alt'];
+					} else {
+						$alt = get_field('home_practice_area_col1]3_heading');
+					}
+				?>
+				<div class="img-holder">
+					<?php if ($svg) : ?>
+						<img src="<?php echo $svg['url'] ?>" onerror="this.src='<?php echo $img['url'] ?>'" alt="<?php echo $alt ?>">
+					<?php else : ?>
+						<img src="<?php echo $img['url'] ?>" alt="<?php echo $alt; ?>">
+					<?php endif; ?>
+				</div>
+				<h4><?php the_field('home_practice_area_col3_heading'); ?></h4>
+				<?php the_field('home_practice_area_col3_content'); ?>
+				<p class="cta-icon-link"><a href="<?php the_field('home_practice_area_col3_link'); ?>"><?php the_field('home_practice_area_col3_link_text'); ?></a></p>
 
-		</section>
+			</section>
+		</div>
 	</section>
 </div>
 <div class="l-contact-form-cta-con l-outer">
