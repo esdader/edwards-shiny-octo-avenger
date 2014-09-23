ea57ead10d47602d965ad0e39ab132d16e400ee3
Tristan Ader <tristanader@gmail.com>
2014-09-23 01:59:37 -0500

    removed reviews custom content type

--- functions.php
+++ functions.php
@@ -115,11 +115,10 @@ require_once('post-types/areas-de-practica.php');
 require_once('post-types/attorneys.php');
 require_once('post-types/blog.php');
 require_once('post-types/case-results.php');
-require_once('post-types/opiniones.php');
+// require_once('post-types/opiniones.php');
 require_once('post-types/practice-areas.php');
 require_once('post-types/resultados-de-casos.php');
-require_once('post-types/reviews.php');
-
+// require_once('post-types/reviews.php');
 
 /**
  * Adding options page
